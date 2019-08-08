39f841257452244e8ce13c932b54b40edfaa5845
Poporacer <46263877+Poporacer@users.noreply.github.com>
2019-08-08 13:18:46 -0600

    Update excluded_plugins.inc.php

--- excluded_plugins.inc.php
+++ excluded_plugins.inc.php
@@ -1,7 +1,8 @@
 <?php
 //put the plugins that should NOT be included in the matrix output
+//this would probably be better if it was changed to be an included array and add items as installed?
 
-$EXCLUDE_PLUGIN_ARRAY = array("FPP-Plugin-Cron-Editor","FPP-Plugin-BetaBriteMessage","FPP-Plugin-BetaBrite","FPP-Plugin-Matrix-Message","fpp-matrixtools","fpp-edmrds","FPP-Plugin-MessageQueue","FPP-Plugin-ProjectorControl","Nagios","LiveMatrix");
+$EXCLUDE_PLUGIN_ARRAY = array("FPP-Plugin-Cron-Editor","FPP-Plugin-BetaBriteMessage","FPP-Plugin-BetaBrite","FPP-Plugin-Matrix-Message","fpp-matrixtools","fpp-edmrds","FPP-Plugin-MessageQueue","FPP-Plugin-ProjectorControl","Nagios","LiveMatrix", fpp-BigButtons");
 
 
 
